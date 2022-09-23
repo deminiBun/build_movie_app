f45bad09941669feb9bf2983cadd48e9096cee94
shuziyoshi <103293229+shuziyoshi@users.noreply.github.com>
2022-09-23 16:17:53 -0700

    Update singlecard_shuzi.js

--- movie_app/movie_app/src/card_shuzi/singlecard_shuzi.js
+++ movie_app/movie_app/src/card_shuzi/singlecard_shuzi.js
@@ -4,3 +4,25 @@ export const API_ENDPOINT = `https://api.themoviedb.org/3/search/movie?api_key=$
 //use it later when we searching for a single Ovie
 
 console.log(API_ENDPOINT);
+
+const MovieList = (props) => {
+  const FavouriteComponent = props.favouriteComponent;
+
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <div className="image-container d-flex justify-content-start m-3">
+          <img src={movie.Poster} alt="movie"></img>
+          <div
+            onClick={() => props.handleFavouritesClick(movie)}
+            className="overlay d-flex align-items-center justify-content-center"
+          >
+            <FavouriteComponent />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieList;
