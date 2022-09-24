3f3e908b71942d9948d04795ee1ee34d28f4a6f5
Abby <qimingjia2021@gmail.com>
2022-09-23 23:58:03 -0700

    add navigation bar

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,11 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router , Route, Link} from 'react-router-dom'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Movie List</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>Movie List</h1>
+        <div className="navigation">
+          <ul>
+            <li><Link to="/home">Home Page</Link></li>
+            <li><Link to="/movie">Movie List</Link></li>
+            <li><Link to="/liked">Movie List of Liked</Link></li>
+            <li><Link to="/blocked">Movie List of Blocked</Link></li>
+          </ul>
+        </div>
+      </div>
+    </Router>
   );
 }
 
