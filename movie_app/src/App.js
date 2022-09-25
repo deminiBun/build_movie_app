03a9d2fcac172cb2272e2764c6116308be314472
Abby <qimingjia2021@gmail.com>
2022-09-25 00:04:57 -0700

    add four components according to the routes

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,22 +1,33 @@
-import "./App.css";
-import { BrowserRouter as Router , Route, Link} from 'react-router-dom'
+import "./App.css"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from "./components/Home/Home"
+import Movie from "./components/Movie/Movie"
+import Liked from "./components/Liked/Liked"
+import Blocked from "./components/Blocked/Blocked"
 
-function App() {
+function App () {
   return (
     <Router>
       <div className='App'>
         <h1>Movie List</h1>
+
         <div className="navigation">
           <ul>
-            <li><Link to="/home">Home Page</Link></li>
+            <li><Link to="/">Home Page</Link></li>
             <li><Link to="/movie">Movie List</Link></li>
             <li><Link to="/liked">Movie List of Liked</Link></li>
             <li><Link to="/blocked">Movie List of Blocked</Link></li>
           </ul>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/movie" element={<Movie />}></Route>
+            <Route path="/liked" element={<Liked />}></Route>
+            <Route path="/blocked" element={<Blocked />}></Route>
+          </Routes>
         </div>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ movie_app/src/components/Home/Home.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+class Home extends React.Component {
+  render () {
+    return <div>首页的内容</div>
+  }
+}
+
+export default Home
